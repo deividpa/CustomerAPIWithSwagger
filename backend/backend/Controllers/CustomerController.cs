d4c728d4c4d80c1f608f3fb2040e2a2357c4294e
David Pérez <deividpa022@gmail.com>
2024-01-03 01:46:11 -0500

    Backend file started using .NET to create the API to handle the customers data

---
+++ backend/backend/Controllers/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using backend.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : Controller
+    {
+        [HttpGet]
+        public async Task<List<CustomerDTO>> GetCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<CustomerDTO> CreateCustomer(CreateCustomerDTO customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<CustomerDTO> GetCustomer(long id) {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public async Task<CustomerDTO> UpdateCustomer(CustomerDTO customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<bool> DeleteCustomer(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ backend/backend/DTOs/CustomerDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace backend.DTOs
+{
+    public class CustomerDTO
+    {
+        public long id {  get; set; } 
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string phone { get; set; }   
+        public string email { get; set; }
+    }
+}
